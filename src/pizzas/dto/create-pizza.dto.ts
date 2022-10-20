16b47a543687d5100087f0781397b1db76a9045a
Benjamin <benjamindeltour22@gmail.com>
2022-10-20 15:35:51 +0200

    Start TP 3

--- src/pizzas/dto/create-pizza.dto.ts
+++ src/pizzas/dto/create-pizza.dto.ts
@@ -1,9 +1,15 @@
+import { IsDefined, IsNotEmpty, MaxLength, Min } from "class-validator";
 import { PizzaFlavor } from "../PizzaFlavor";
 
 export class CreatePizzaDto {
+    @IsNotEmpty()
+    @MaxLength(100)
     name: string;
+    @IsDefined()
     flavor: PizzaFlavor;
     speciality: boolean;
+    @IsDefined()
+    @Min(0)
     price: number;
     end?: Date;
 }
