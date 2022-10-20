a64f2b84f5a376f53399ecee7a8a785dc2bd25e5
Benjamin <benjamindeltour22@gmail.com>
2022-10-20 12:01:56 +0200

    ORM install

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import {ConfigModule} from '@nestjs/config';
+import {TypeOrmModule} from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PizzasModule } from './pizzas/pizzas.module';
@@ -7,6 +8,16 @@ import { PizzasModule } from './pizzas/pizzas.module';
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'BenDel',
+      password: 'WolfDry*2412',
+      database: 'pizzapi',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
     PizzasModule,
   ],
   controllers: [AppController],
