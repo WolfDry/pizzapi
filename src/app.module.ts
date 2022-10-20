4f4014cef4693c5167759597f10ad10875a77f25
Benjamin <benjamindeltour22@gmail.com>
2022-10-20 16:29:13 +0200

    Start TP 4

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import {ConfigModule} from '@nestjs/config';
 import {TypeOrmModule} from '@nestjs/typeorm';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PizzasModule } from './pizzas/pizzas.module';
@@ -10,13 +11,18 @@ import { PizzasModule } from './pizzas/pizzas.module';
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'BenDel',
-      password: 'WolfDry*2412',
-      database: 'pizzapi',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       autoLoadEntities: true,
       synchronize: true,
+      migrationsRun: true,
+      migrationsTableName: 'migration_history',
+      migrations: [
+        join(__dirname, 'migration/*{.ts,.js}')
+      ]
     }),
     PizzasModule,
   ],
---
+++ src/migrations/1666275879253-migrations.ts
@@ -0,0 +1,24 @@
+import { Pizza } from "src/pizzas/entities/pizza.entity"
+import { PizzaFlavor } from "src/pizzas/PizzaFlavor";
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class migrations1666275879253 implements MigrationInterface {
+
+    public async up(run: QueryRunner): Promise<void> {
+        await run.manager.save(run.manager.create<Pizza>(Pizza,{
+            name: 'Reine',
+            flavor: PizzaFlavor.Tomatoes,
+            price: 13
+        }));
+        await run.manager.save(run.manager.create<Pizza>(Pizza,{
+            name: '4 formages',
+            flavor: PizzaFlavor.Crema,
+            price: 15
+        }));
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query('DELETE * from currency')
+    }
+
+}
