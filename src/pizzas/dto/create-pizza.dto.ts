6c1917dc860e42b816d014ebb8148800ebf92c78
Benjamin <benjamindeltour22@gmail.com>
2022-10-20 12:23:21 +0200

    Create pizza entity

--- src/pizzas/dto/create-pizza.dto.ts
+++ src/pizzas/dto/create-pizza.dto.ts
@@ -1,1 +1,9 @@
-export class CreatePizzaDto {}
+import { PizzaFlavor } from "../entities/pizza.entity";
+
+export class CreatePizzaDto {
+    name: string;
+    flavor: PizzaFlavor;
+    speciality: boolean;
+    price: number;
+    end?: Date;
+}
--- src/pizzas/entities/pizza.entity.ts
+++ src/pizzas/entities/pizza.entity.ts
@@ -1,1 +1,34 @@
-export class Pizza {}
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
+
+export enum PizzaFlavor{
+    TOMATOES = 'tomatoes',
+    CHEESE = 'cheese',
+    OLIVE = 'olive'
+}
+
+@Entity()
+export class Pizza {
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @Column({
+        length: 100,
+        unique: true
+    })
+    name!: string;
+
+    @Column({type: 'enum', enum: PizzaFlavor})
+    flavor!: PizzaFlavor;
+
+    @Column({type: 'timestamp', default: () => 'current_timestamp'})
+    created!: Date;
+
+    @Column({default: false})
+    speciality!: boolean;
+
+    @Column()
+    price!: number;
+
+    @Column({nullable: true})
+    end?: Date;
+}
