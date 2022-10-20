6cdce8837bcef4189ca13d30dd740e2b8404df38
Benjamin <benjamindeltour22@gmail.com>
2022-10-20 16:47:30 +0200

    Start TP 5

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,26 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger/dist';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //Acitvation de la validation
   app.useGlobalPipes(new ValidationPipe());
+
+  //Activation du swagger
+  const config = new DocumentBuilder()
+    .setTitle('PIZZAPI api')
+    .setDescription('The pizza API description')
+    .setVersion('1.0')
+    .addTag('pizza')
+    .build()
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc', app, document);
+
+  //Ecoute du port
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
--- src/pizzas/pizzas.controller.ts
+++ src/pizzas/pizzas.controller.ts
@@ -2,31 +2,58 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { PizzasService } from './pizzas.service';
 import { CreatePizzaDto } from './dto/create-pizza.dto';
 import { UpdatePizzaDto } from './dto/update-pizza.dto';
+import { ApiOperation, ApiCreatedResponse, ApiNotFoundResponse, ApiOkResponse } from '@nestjs/swagger';
+import { Pizza } from './entities/pizza.entity';
 
 @Controller('pizzas')
 export class PizzasController {
   constructor(private readonly pizzasService: PizzasService) {}
 
+  @ApiOperation({description:"Add a pizza to the menu"})
+  @ApiCreatedResponse({
+    description: 'The record has been successfully created.',
+    type: Pizza
+  })
   @Post()
   create(@Body() createPizzaDto: CreatePizzaDto) {
     return this.pizzasService.create(createPizzaDto);
   }
 
+  @ApiOperation({description:"List all pizzas in the menu"})
+  @ApiOkResponse({
+    description: 'All pizzas',
+    type: [Pizza]
+  })
   @Get()
   findAll() {
     return this.pizzasService.findAll();
   }
 
+  @ApiOperation({description:"Find a pizza in the menu"})
+  @ApiNotFoundResponse({description: "The pizza does not exist"})
+  @ApiOkResponse({
+    description: 'The pizza',
+    type: Pizza
+  })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.pizzasService.findOne(+id);
   }
 
+  @ApiOperation({description:"Modify a pizza in the menu"})
+  @ApiNotFoundResponse({description: "The pizza does not exist"})
+  @ApiOkResponse({
+    description: 'The patched pizzas',
+    type: Pizza
+  })
   @Patch(':id')
   update(@Param('id') id: string, @Body() updatePizzaDto: UpdatePizzaDto) {
     return this.pizzasService.update(+id, updatePizzaDto);
   }
 
+  @ApiOperation({description:"Remove a pizza in the menu"})
+  @ApiNotFoundResponse({description: "The pizza does not exist"})
+  @ApiOkResponse({description: "The pizza was removed"})
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.pizzasService.remove(+id);
